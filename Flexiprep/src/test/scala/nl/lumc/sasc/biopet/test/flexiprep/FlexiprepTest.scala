d276f58d9659358127c1478b9ca0a8e6abd285d9
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-08-05 19:32:13 +0200

    Added paired test

--- Flexiprep/src/test/scala/nl/lumc/sasc/biopet/test/flexiprep/FlexiprepTest.scala
+++ Flexiprep/src/test/scala/nl/lumc/sasc/biopet/test/flexiprep/FlexiprepTest.scala
@@ -9,5 +9,14 @@ import org.scalatest.Matchers
  * Created by pjvan_thof on 7/6/15.
  */
 class FlexiprepSingleTest extends AbstractFlexiprep with Matchers {
-  override def args = super.args ++ Seq("-R1", Biopet.fixtureFile("flexiprep" + File.separator + "ct_r1.fq.gz").getAbsolutePath)
+  override def args = super.args ++ Seq(
+    "-R1", Biopet.fixtureFile("flexiprep" + File.separator + "ct_r1.fq.gz").getAbsolutePath
+  )
+}
+
+class FlexiprepPairedTest extends AbstractFlexiprep with Matchers {
+  override def args = super.args ++ Seq(
+    "-R1", Biopet.fixtureFile("flexiprep" + File.separator + "ct_r1.fq.gz").getAbsolutePath,
+    "-R2", Biopet.fixtureFile("flexiprep" + File.separator + "ct_r2.fq.gz").getAbsolutePath
+  )
 }
