cf78892ee21dc64541799bc3cd059d217cd0806e
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-08-05 19:06:17 +0200

    Added option to override memory of pipeline

--- Common/src/main/scala/nl/lumc/sasc/biopet/test/Pipeline.scala
+++ Common/src/main/scala/nl/lumc/sasc/biopet/test/Pipeline.scala
@@ -5,14 +5,19 @@ import java.io.{ PrintWriter, File }
 import org.scalatest.Matchers
 import org.scalatest.testng.TestNGSuite
 import org.testng.annotations.{ Test, BeforeClass }
+import sun.rmi.log.ReliableLog.LogFile
 
+import scala.io.Source
 import scala.sys.process._
 
 /**
  * Created by pjvan_thof on 6/30/15.
  */
+
 trait Pipeline extends TestNGSuite with Matchers {
   val outputDir = new File(Biopet.getOutputDir, this.getClass.getName)
+
+  val logFile = new File(outputDir, "run.log")
 
   def pipelineName: String
 
@@ -21,24 +26,27 @@ trait Pipeline extends TestNGSuite with Matchers {
   private var _exitValue = -1
   def exitValue = _exitValue
 
+  def memoryArg = "-Xmx200m"
+
   @BeforeClass
   def beforeTest: Unit = {
     // Running pipeline
-    _exitValue = Pipeline.runPipeline(pipelineName, outputDir, args)
+    _exitValue = Pipeline.runPipeline(pipelineName, outputDir, args, logFile, memoryArg)
   }
 
   @Test(priority = -1) def exitcode = exitValue shouldBe 0
   @Test def outputDirExist = assert(outputDir.exists())
-  @Test def logFileExist = assert(new File(outputDir, "run.log").exists())
-
+  @Test def logFileExist = assert(logFile.exists())
 }
 
 object Pipeline {
-  def runPipeline(pipelineName: String, outputDir: File, args: Seq[String]) = {
-    val cmd = Seq("java", "-Xmx512m", "-jar", Biopet.getBiopetJar.toString, "pipeline", pipelineName) ++ args ++ Biopet.queueArgs
+  def runPipeline(pipelineName: String,
+                  outputDir: File, args: Seq[String],
+                  logFile: File,
+                  memoryArg: String) = {
+    val cmd = Seq("java", memoryArg, "-jar", Biopet.getBiopetJar.toString, "pipeline", pipelineName) ++ args ++ Biopet.queueArgs
     if (!outputDir.exists()) outputDir.mkdir()
 
-    val logFile = new File(outputDir, "run.log")
     if (logFile.exists()) logFile.delete()
     val writer = new PrintWriter(logFile)
     def writeLine(line: String): Unit = {
