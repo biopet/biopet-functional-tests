e3d37e66094ce9abbe402e407066245d09214803
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-08-07 12:09:57 +0200

    Adding basic mapping tests

---
+++ Mapping/src/test/scala/nl/lumc/sasc/biopet/test/mapping/MappingTest.scala
@@ -0,0 +1,22 @@
+package nl.lumc.sasc.biopet.test.mapping
+
+import java.io.File
+
+import nl.lumc.sasc.biopet.test.Biopet
+import org.scalatest.Matchers
+
+/**
+ * Created by pjvan_thof on 7/6/15.
+ */
+class MappingSingleTest extends AbstractMapping {
+  override def args = super.args ++ Seq(
+    "-R1", Biopet.fixtureFile("flexiprep" + File.separator + "ct_r1.fq.gz").getAbsolutePath
+  )
+}
+
+class MappingPairedTest extends AbstractMapping {
+  override def args = super.args ++ Seq(
+    "-R1", Biopet.fixtureFile("flexiprep" + File.separator + "ct_r1.fq.gz").getAbsolutePath,
+    "-R2", Biopet.fixtureFile("flexiprep" + File.separator + "ct_r2.fq.gz").getAbsolutePath
+  )
+}
