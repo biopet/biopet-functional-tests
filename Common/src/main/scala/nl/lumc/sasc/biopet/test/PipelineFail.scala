9d10270a5630db6149941be1b4b9e0791a7bbf90
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-09-19 12:33:32 +0200

    Adding trait to test failing pipeline

---
+++ Common/src/main/scala/nl/lumc/sasc/biopet/test/PipelineFail.scala
@@ -0,0 +1,10 @@
+package nl.lumc.sasc.biopet.test
+
+import org.testng.annotations.Test
+
+/**
+ * Created by pjvanthof on 19/09/15.
+ */
+trait PipelineFail extends Pipeline {
+  @Test(priority = -1) override def exitcode = exitValue should not be 0
+}
