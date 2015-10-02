c5f256d91bb10dea4d506cbe261aa522a6ec4e01
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-10-02 13:38:21 +0200

    Change to summary

--- Flexiprep/src/test/scala/nl/lumc/sasc/biopet/test/flexiprep/FlexiprepRunPaired.scala
+++ Flexiprep/src/test/scala/nl/lumc/sasc/biopet/test/flexiprep/FlexiprepRunPaired.scala
@@ -1,17 +1,35 @@
 package nl.lumc.sasc.biopet.test.flexiprep
 
+import java.io.File
+
 import org.testng.annotations.Test
 import nl.lumc.sasc.biopet.test.utils._
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
 
 class FlexiprepRunPaired extends FlexiprepRunSingle {
 
-  @Test def testOutputR1() = {
-    val outputR1 = getOutputFile("ct_r1.qc.fq.gz")
-    assert(calcMd5Unzipped(outputR1) == "b6f564f7496039dfe4e4e9794d191af2")
+  @Test(dependsOnGroups = Array("parseSummary"))
+  def testOutputR1() = {
+    val summaryFile = summary \ "samples" \ sampleId \ "libraries" \ libId \ "flexiprep" \ "files" \ "output_R1"
+    (summaryFile \ "path") shouldBe JString
+    (summaryFile \ "md5") shouldBe JString
+    val file = new File((summaryFile \ "path").extract[String])
+    val md5 = calcMd5Unzipped(file)
+
+    md5 shouldBe "b6f564f7496039dfe4e4e9794d191af2"
+    md5 shouldBe (summaryFile \ "md5").extract[String]
   }
 
-  @Test def testOutputR2() = {
-    val outputR1 = getOutputFile("ct_r1.qc.fq.gz")
-    assert(calcMd5Unzipped(outputR1) == "707e26b2fb5a2c999783a2830931f952")
+  @Test(dependsOnGroups = Array("parseSummary"))
+  def testOutputR2() = {
+    val summaryFile = summary \ "samples" \ sampleId \ "libraries" \ libId \ "flexiprep" \ "files" \ "output_R2"
+    (summaryFile \ "path") shouldBe JString
+    (summaryFile \ "md5") shouldBe JString
+    val file = new File((summaryFile \ "path").extract[String])
+    val md5 = calcMd5Unzipped(file)
+
+    md5 shouldBe "707e26b2fb5a2c999783a2830931f952"
+    md5 shouldBe (summaryFile \ "md5").extract[String]
   }
 }
