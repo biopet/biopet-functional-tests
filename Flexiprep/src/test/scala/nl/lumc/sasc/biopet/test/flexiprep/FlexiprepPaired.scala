9d63f399693da6444b9c60eb5211e19f21e1732a
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-10-04 11:08:01 +0200

    Added tests with unzipped input

--- Flexiprep/src/test/scala/nl/lumc/sasc/biopet/test/flexiprep/FlexiprepPaired.scala
+++ Flexiprep/src/test/scala/nl/lumc/sasc/biopet/test/flexiprep/FlexiprepPaired.scala
@@ -44,3 +44,18 @@ class FlexiprepPairedTrimTest extends FlexiprepPaired {
 class FlexiprepPairedRemoveOutputTest extends FlexiprepPairedClipTrimTest {
   override def keepQcFastqFiles = false
 }
+
+class FlexiprepPairedUnzippedTest extends FlexiprepPairedClipTrimTest {
+  /** Input file of this run. */
+  override def r1 = Some(new File(Biopet.fixtureFile("flexiprep" + File.separator + "ct_r1.fq").getAbsolutePath))
+
+  /** MD5 checksum of the input file. */
+  override def md5SumInputR1 = "b6f564f7496039dfe4e4e9794d191af2"
+
+  /** Input read pair 2 for this run. */
+  override def r2 = Some(new File(Biopet.fixtureFile("flexiprep" + File.separator + "ct_r2.fq").getAbsolutePath))
+
+  /** MD5 checksum of input read pair 2. */
+  override def md5SumInputR2 = Some("707e26b2fb5a2c999783a2830931f952")
+
+}
--- Flexiprep/src/test/scala/nl/lumc/sasc/biopet/test/flexiprep/FlexiprepSingle.scala
+++ Flexiprep/src/test/scala/nl/lumc/sasc/biopet/test/flexiprep/FlexiprepSingle.scala
@@ -39,3 +39,11 @@ class FlexiprepSingleTrimTest extends FlexiprepRunSingle {
 class FlexiprepSingleRemoveOutputTest extends FlexiprepSingleClipTrimTest {
   override def keepQcFastqFiles = false
 }
+
+class FlexiprepSingleUnzippedTest extends FlexiprepSingleClipTrimTest {
+  /** Input file of this run. */
+  override def r1 = Some(new File(Biopet.fixtureFile("flexiprep" + File.separator + "ct_r1.fq").getAbsolutePath))
+
+  /** MD5 checksum of the input file. */
+  override def md5SumInputR1 = "b6f564f7496039dfe4e4e9794d191af2"
+}
