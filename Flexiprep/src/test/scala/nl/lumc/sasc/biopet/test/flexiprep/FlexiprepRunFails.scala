d31596f9174c5922322540d425d98f48f392e2d4
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-10-05 17:01:37 +0200

    Added dry run tests for failing pipeline tests

--- Flexiprep/src/test/scala/nl/lumc/sasc/biopet/test/flexiprep/FlexiprepRunFails.scala
+++ Flexiprep/src/test/scala/nl/lumc/sasc/biopet/test/flexiprep/FlexiprepRunFails.scala
@@ -6,7 +6,11 @@ import nl.lumc.sasc.biopet.test.{ Biopet, PipelineFail }
 import org.testng.annotations.Test
 
 class FlexiprepNoR1ArgTest extends FlexiprepRun with PipelineFail {
-  override def args = super.args ++ Seq("-run")
+  logMustHave("""Argument with name '--input_r1' (-R1) is missing""".r)
+}
+
+class FlexiprepDryRunNoR1ArgTest extends FlexiprepRun with PipelineFail {
+  override def run = false
   logMustHave("""Argument with name '--input_r1' (-R1) is missing""".r)
 }
 
@@ -18,6 +22,23 @@ class FlexiprepR1NotExistTest extends FlexiprepRun with PipelineFail {
 }
 
 class FlexiprepR2NotExistTest extends FlexiprepRun with PipelineFail {
+  override def r1 = Some(new File(Biopet.fixtureFile("flexiprep" + File.separator + "ct_r1.fq.gz").getAbsolutePath))
+  override def r2 = Some(new File(Biopet.fixtureDir, "this_should_not_exist.R2.fq"))
+  logMustNotHave("""FunctionEdge - Starting""".r)
+
+  @Test def fileNotExist = assert(!r2.get.exists(), s"This file should not exist: $r2")
+}
+
+class FlexiprepDryRunR1NotExistTest extends FlexiprepRun with PipelineFail {
+  override def run = false
+  override def r1 = Some(new File(Biopet.fixtureDir, "This_should_not_exist.R1.fq"))
+  logMustNotHave("""FunctionEdge - Starting""".r)
+
+  @Test def fileNotExist = assert(!r1.get.exists(), s"this file should not exist: $r1")
+}
+
+class FlexiprepDryRunR2NotExistTest extends FlexiprepRun with PipelineFail {
+  override def run = false
   override def r1 = Some(new File(Biopet.fixtureFile("flexiprep" + File.separator + "ct_r1.fq.gz").getAbsolutePath))
   override def r2 = Some(new File(Biopet.fixtureDir, "this_should_not_exist.R2.fq"))
   logMustNotHave("""FunctionEdge - Starting""".r)
