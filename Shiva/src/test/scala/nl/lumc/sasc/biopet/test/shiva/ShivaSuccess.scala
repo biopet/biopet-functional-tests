34254c79afdbc8a3bb716bf451aefa3c647ebcfe
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-10-19 19:56:57 +0200

    Adding seperated tests for all variantcallers

--- Shiva/src/test/scala/nl/lumc/sasc/biopet/test/shiva/ShivaSuccess.scala
+++ Shiva/src/test/scala/nl/lumc/sasc/biopet/test/shiva/ShivaSuccess.scala
@@ -3,11 +3,27 @@ package nl.lumc.sasc.biopet.test.shiva
 import java.io.File
 
 import nl.lumc.sasc.biopet.test.MultisampleSuccess
-import org.testng.annotations.Test
+import org.json4s._
+import org.testng.annotations.{DataProvider, Test}
 
 /**
  * Created by pjvan_thof on 9/17/15.
  */
 trait ShivaSuccess extends Shiva with MultisampleSuccess {
+  @DataProvider(name = "variantcallers")
+  def variantcallerProvider = Shiva.validVariantcallers.map(Array(_)).toArray
 
+  @Test(dataProvider = "variantcallers", dependsOnGroups = Array("parseSummary"))
+  def testVariantcaller(variantcaller: String): Unit = {
+    val dir = new File(outputDir, "variantcalling" + File.separator + variantcaller)
+    val vcfstats = this.summary \ "shivavariantcalling" \ s"multisample-vcfstats-$variantcaller"
+    if (!multisampleVariantcalling.contains(false) && variantcallers.contains(variantcaller)) {
+      assert(dir.exists())
+      assert(dir.isDirectory)
+      vcfstats shouldBe a[JObject]
+    } else {
+      assert(!dir.exists())
+      vcfstats shouldBe JNothing
+    }
+  }
 }
--- Shiva/src/test/scala/nl/lumc/sasc/biopet/test/shiva/ShivaTest.scala
+++ Shiva/src/test/scala/nl/lumc/sasc/biopet/test/shiva/ShivaTest.scala
@@ -6,9 +6,32 @@ import nl.lumc.sasc.biopet.test.utils._
 /**
  * Created by pjvan_thof on 10/1/15.
  */
-class ShivaTest extends ShivaSuccess with TestReference {
-  val configFile = createTempConfig(Map("variantcallers" -> List("haplotypecaller", "haplotypecaller_gvcf")))
-  override def configs = super.configs ::: Samples.wgs1Config :: configFile :: Nil
+trait ShivaWgs1 extends ShivaSuccess with TestReference {
+  override def configs = super.configs ::: Samples.wgs1Config :: Nil
 
   def samples = Map("wgs1" -> List("lib1"))
+}
+
+class ShivaHaplotypeCaller extends ShivaWgs1 {
+  override def variantcallers = List("haplotypecaller")
+}
+
+class ShivaHaplotypeCallerGvcf extends ShivaWgs1 {
+  override def variantcallers = List("haplotypecaller_gvcf")
+}
+
+class ShivaUnifiedGenotyper extends ShivaWgs1 {
+  override def variantcallers = List("unifiedgenotyper")
+}
+
+class ShivaBcftools extends ShivaWgs1 {
+  override def variantcallers = List("bcftools")
+}
+
+class ShivaFreebayes extends ShivaWgs1 {
+  override def variantcallers = List("freebayes")
+}
+
+class ShivaRaw extends ShivaWgs1 {
+  override def variantcallers = List("raw")
 }
