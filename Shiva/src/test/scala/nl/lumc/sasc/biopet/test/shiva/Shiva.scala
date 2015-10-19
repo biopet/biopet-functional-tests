6912edb346e30bf4c949ab72a913ae4fc7045d08
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-10-19 19:56:08 +0200

    Adding commandline options

--- Shiva/src/test/scala/nl/lumc/sasc/biopet/test/shiva/Shiva.scala
+++ Shiva/src/test/scala/nl/lumc/sasc/biopet/test/shiva/Shiva.scala
@@ -4,6 +4,7 @@ import java.io.{ PrintWriter, File }
 
 import nl.lumc.sasc.biopet.test.Pipeline
 import nl.lumc.sasc.biopet.test.Pipeline._
+import nl.lumc.sasc.biopet.test.utils._
 
 /**
  * Created by pjvan_thof on 5/26/15.
@@ -20,8 +21,49 @@ trait Shiva extends Pipeline {
 
   def summaryFile = new File(outputDir, s"Shiva.summary.json")
 
+  def bamToFastq: Option[Boolean] = None
+
+  def correctReadgroups: Option[Boolean] = None
+
+  def libraryVariantcalling: Option[Boolean] = None
+
+  def singleSampleVariantcalling: Option[Boolean] = None
+
+  def multisampleVariantcalling: Option[Boolean] = None
+
+  def svCalling: Option[Boolean] = None
+
+  def useIndelRealigner: Option[Boolean] = None
+
+  def useBaseRecalibration: Option[Boolean] = None
+
+  def useAnalyzeCovariates: Option[Boolean] = None
+
+  def dbsnpVcfFile: Option[File] = None
+
+  def variantcallers: List[String] = Nil
+
+  val variantcallersConfig = if (variantcallers.nonEmpty) Some(createTempConfig(Map("variantcallers" -> variantcallers))) else None
+
+  override def configs = super.configs ::: variantcallersConfig.map(_ :: Nil).getOrElse(Nil)
+
   def args = cmdConfig("species", referenceSpecies) ++
     cmdConfig("reference_name", referenceName) ++
-    cmdConfig("aligner", aligner)
+    cmdConfig("aligner", aligner) ++
+    cmdConfig("bam_to_fastq", bamToFastq) ++
+    cmdConfig("correct_readgroups", correctReadgroups) ++
+    cmdConfig("library_variantcalling", libraryVariantcalling) ++
+    cmdConfig("single_sample_variantcalling", singleSampleVariantcalling) ++
+    cmdConfig("multisample_variantcalling", multisampleVariantcalling) ++
+    cmdConfig("sv_calling", svCalling) ++
+    cmdConfig("use_indel_realigner", useIndelRealigner) ++
+    cmdConfig("use_base_recalibration", useBaseRecalibration) ++
+    cmdConfig("use_analyze_covariates", useAnalyzeCovariates) ++
+    cmdConfig("dbsnp", dbsnpVcfFile)
 }
 
+object Shiva {
+  val validVariantcallers = List("freebayes", "raw", "bcftools",
+    "haplotypecaller", "unifiedgenotyper", "haplotypecaller_gvcf",
+    "haplotypecaller_allele", "unifiedgenotyper_allele")
+}
