29ebc1e3e552397c33eed07b8ae28f6b57b5caa8
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-10-20 13:42:14 +0200

    Fix outputDir test

--- Shiva/src/test/scala/nl/lumc/sasc/biopet/test/shiva/ShivaFails.scala
+++ Shiva/src/test/scala/nl/lumc/sasc/biopet/test/shiva/ShivaFails.scala
@@ -6,17 +6,21 @@ import nl.lumc.sasc.biopet.test.{ TestReference, Samples, PipelineFail }
  * Created by pjvanthof on 19/10/15.
  */
 class ShivaNoSamplesTest extends Shiva with PipelineFail {
+  override def variantcallers = "haplotypecaller" :: Nil
   logMustNotHave("""FunctionEdge - Starting""".r)
   logMustHave("""No Samples found in config""".r)
 }
 
 class ShivaNoReferenceTest extends Shiva with PipelineFail {
+  override def variantcallers = "haplotypecaller" :: Nil
   override def configs = super.configs ::: Samples.wgs1Config :: Nil
   logMustNotHave("""FunctionEdge - Starting""".r)
   logMustHave("""Value does not exist but is required, key: reference_fasta""".r)
 }
 
 class ShivaNoOutputDirTest extends Shiva with PipelineFail with TestReference {
+  override def variantcallers = "haplotypecaller" :: Nil
+  override def outputDirArg = None
   override def configs = super.configs ::: Samples.wgs1Config :: Nil
   logMustNotHave("""FunctionEdge - Starting""".r)
   logMustHave("""Value does not exist but is required, key: output_dir""".r)
