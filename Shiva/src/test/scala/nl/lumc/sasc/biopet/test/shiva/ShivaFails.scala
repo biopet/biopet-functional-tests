8691e40e25c40b85e76af9b3758cdbcc0d625ace
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-10-26 09:47:21 +0100

    Adding test on md5 abort

--- Shiva/src/test/scala/nl/lumc/sasc/biopet/test/shiva/ShivaFails.scala
+++ Shiva/src/test/scala/nl/lumc/sasc/biopet/test/shiva/ShivaFails.scala
@@ -1,6 +1,7 @@
 package nl.lumc.sasc.biopet.test.shiva
 
 import nl.lumc.sasc.biopet.test.{ TestReference, Samples, PipelineFail }
+import nl.lumc.sasc.biopet.test.Pipeline._
 
 /**
  * Created by pjvanthof on 19/10/15.
@@ -30,4 +31,12 @@ class ShivaWrongBamHeader extends Shiva with PipelineFail with TestReference {
   override def configs = super.configs ::: Samples.wgs1BamWrongHeaderConfig :: Nil
   //TODO: Add log checks
   logMustNotHave("""FunctionEdge - Starting""".r)
+}
+
+class ShivaWrongMd5Test extends Shiva with PipelineFail {
+  override def variantcallers = "haplotypecaller" :: Nil
+  override def configs = super.configs ::: Samples.wgs1Config :: Nil
+  override def args = super.args ++ cmdConfig("samples:wgs1:libraries:lib1:R1_md5", "this_should_not_match_md5")
+  logMustHave("""FunctionEdge - Starting""".r)
+  //TODO: Add log checks
 }
