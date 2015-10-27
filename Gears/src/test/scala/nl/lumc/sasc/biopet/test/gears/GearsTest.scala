86778a318826fc46cc1bd6cf0eff29aea59dd45e
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-10-27 21:35:00 +0100

    Fix class names

--- Gears/src/test/scala/nl/lumc/sasc/biopet/test/gears/GearsTest.scala
+++ Gears/src/test/scala/nl/lumc/sasc/biopet/test/gears/GearsTest.scala
@@ -7,11 +7,11 @@ import nl.lumc.sasc.biopet.test.Biopet
 /**
  * Created by pjvanthof on 27/10/15.
  */
-class GearsBamTest extends GearsSuccessful {
+class GearsFastqTest extends GearsSuccessful {
   override def r1 = Some(Biopet.fixtureFile("flexiprep" + File.separator + "ct_r1.fq.gz"))
   override def r2 = Some(Biopet.fixtureFile("flexiprep" + File.separator + "ct_r2.fq.gz"))
 }
 
-class GearsFastqTest extends GearsSuccessful {
+class GearsBamTest extends GearsSuccessful {
   override def bam = Some(new File(Biopet.fixtureFile("gears" + File.separator + "hpv_simu.sorted.bam").getAbsolutePath))
 }
