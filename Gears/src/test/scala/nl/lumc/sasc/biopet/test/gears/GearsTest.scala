30a327a83ca117a79ad3fea283a0f312a443ac23
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-10-30 12:44:55 +0100

    Fix input files

--- Gears/src/test/scala/nl/lumc/sasc/biopet/test/gears/GearsTest.scala
+++ Gears/src/test/scala/nl/lumc/sasc/biopet/test/gears/GearsTest.scala
@@ -8,10 +8,10 @@ import nl.lumc.sasc.biopet.test.Biopet
  * Created by pjvanthof on 27/10/15.
  */
 class GearsFastqTest extends GearsSuccessful {
-  override def r1 = Some(Biopet.fixtureFile("flexiprep" + File.separator + "ct_r1.fq.gz"))
-  override def r2 = Some(Biopet.fixtureFile("flexiprep" + File.separator + "ct_r2.fq.gz"))
+  override def r1 = Some(Biopet.fixtureFile("gears" + File.separator + "hpv_simu_R1.fq.gz"))
+  override def r2 = Some(Biopet.fixtureFile("gears" + File.separator + "hpv_simu_R2.fq.gz"))
 }
 
 class GearsBamTest extends GearsSuccessful {
-  override def bam = Some(new File(Biopet.fixtureFile("gears" + File.separator + "hpv_simu.sorted.bam").getAbsolutePath))
+  override def bam = Some(new File(Biopet.fixtureFile("gears" + File.separator + "hpv_simu-testlib.dedup.bam").getAbsolutePath))
 }
