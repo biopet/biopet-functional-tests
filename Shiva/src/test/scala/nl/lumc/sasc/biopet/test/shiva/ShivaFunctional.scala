19b5939d12fb165370ad78a676a00bfff599efa4
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-11-02 06:39:43 +0100

    Changed input files

--- Shiva/src/test/scala/nl/lumc/sasc/biopet/test/shiva/ShivaFunctional.scala
+++ Shiva/src/test/scala/nl/lumc/sasc/biopet/test/shiva/ShivaFunctional.scala
@@ -2,7 +2,7 @@ package nl.lumc.sasc.biopet.test.shiva
 
 import java.io.File
 
-import nl.lumc.sasc.biopet.test.{Biopet, Samples}
+import nl.lumc.sasc.biopet.test.{ Biopet, Samples }
 
 /**
  * Created by pjvanthof on 01/11/15.
@@ -11,7 +11,7 @@ class ShivaBiopetplanet30xTest extends ShivaSuccess {
   override def referenceSpecies = Some("H.sapiens")
   override def referenceName = Some("GRCh37")
 
-  override def variantcallers = List("haplotypecaller" ,"haplotypecaller_gvcf", "unifiedgenotyper")
+  override def variantcallers = List("haplotypecaller", "haplotypecaller_gvcf", "unifiedgenotyper")
 
   override def configs = super.configs ::: Samples.na12878Gatc30xConfig :: Nil
 
@@ -19,8 +19,14 @@ class ShivaBiopetplanet30xTest extends ShivaSuccess {
 
   def samples = Map("NA12878" -> List("biopetplanet-30x"))
 
-  override def ampliconBed = Some(Biopet.fixtureFile(List("samples", "NA12878", "biopetplanet-30x", "gcat_set_025.GRCh37.bed").mkString(File.separator)))
+  override def ampliconBed =
+    Some(Biopet.fixtureFile(List("samples", "NA12878", "biopetplanet-30x", "gcat_set_025.bed")
+      .mkString(File.separator)))
 
-  override def referenceVcf = Some(Biopet.fixtureFile(List("samples", "NA12878", "NISTIntegratedCalls_14datasets_131103_allcall_UGHapMerge_HetHomVarPASS_VQSRv2.19_2mindatasets_5minYesNoRatio_all_nouncert_excludesimplerep_excludesegdups_excludedecoy_excludeRepSeqSTRs_noCNVs.vcf.gz").mkString(File.separator)))
-  override def referenceVcfRegions = Some(Biopet.fixtureFile(List("samples", "NA12878", "biopetplanet-30x", "gcat_set_025.GRCh37.confidence.bed").mkString(File.separator)))
+  override def referenceVcf =
+    Some(Biopet.fixtureFile(List("samples", "NA12878", "giab.v2.19.confidence.bed.gz")
+      .mkString(File.separator)))
+  override def referenceVcfRegions =
+    Some(Biopet.fixtureFile(List("samples", "NA12878", "biopetplanet-30x", "gcat_set_025.confidence.bed")
+      .mkString(File.separator)))
 }
