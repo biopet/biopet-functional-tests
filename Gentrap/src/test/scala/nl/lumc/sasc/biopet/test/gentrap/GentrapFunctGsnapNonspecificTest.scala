a83f9645021dee7460755d31be1d3084f8ab5ab1
bow <bow@bow.web.id>
2015-11-13 13:53:37 +0100

    Initial count table test

--- Gentrap/src/test/scala/nl/lumc/sasc/biopet/test/gentrap/GentrapFunctGsnapNonspecificTest.scala
+++ Gentrap/src/test/scala/nl/lumc/sasc/biopet/test/gentrap/GentrapFunctGsnapNonspecificTest.scala
@@ -1,11 +1,25 @@
 package nl.lumc.sasc.biopet.test.gentrap
 
+import java.io.File
+
+import org.testng.annotations.Test
+
 import nl.lumc.sasc.biopet.test.{ Biopet, Pipeline, Samples }, Pipeline._
+import nl.lumc.sasc.biopet.test.utils.pearsonScore
 
 /** Functional test for Gentrap with complete options, using gsnap and non_specific strand protocol. */
 class GentrapFunctGsnapNonspecificTest extends GentrapSuccess with GentrapRefSeq {
 
   override def functionalTest = true
+
+  // TODO: Map[String, Map[String, Double]] for the count table instead?
+  def loadMergedCountTable(tableFile: File): Map[String, Seq[Double]] = {
+    val lineIter = scala.io.Source.fromFile(tableFile).getLines()
+    val header = lineIter.next()
+    val sampleNames = header.stripLineEnd.split("\t").tail.toSeq
+    val counts = lineIter.map(_.stripLineEnd.split("\t").tail.toSeq.map(_.toDouble))
+    sampleNames.zip(counts.toSeq).toMap
+  }
 
   def samples = Map("rna1" -> List("lib1"))
 
@@ -26,4 +40,23 @@ class GentrapFunctGsnapNonspecificTest extends GentrapSuccess with GentrapRefSeq
   override def args = super.args ++
     cmdConfig("dir", Biopet.fixtureFile("gentrap").getAbsolutePath) ++
     cmdConfig("db", "hg19_mini")
+
+  def fragmentsPerGene: File = new File(outputDir, "all_samples.fragments_per_gene")
+
+  @Test def fragmentsPerGeneTest(): Unit = {
+    val refFragmentsPerGene = Biopet.fixtureFile("gentrap", "count_table", "func1", "all_samples.fragments_per_gene")
+
+    val tableA = loadMergedCountTable(refFragmentsPerGene)
+    val tableB = loadMergedCountTable(fragmentsPerGene)
+
+    tableB.size shouldBe tableA.size
+    tableB.keySet shouldEqual tableA.keySet
+
+    val samples = tableB.keySet
+    samples.foreach { sample =>
+      val tableAValues = tableA(sample).toList
+      val tableBValues = tableB(sample).toList
+      pearsonScore(tableAValues, tableBValues).getOrElse(0.0) should be >= 0.999
+    }
+  }
 }
