ea551b8a8c9bd0ab569a74e47bd1347819cee808
bow <bow@bow.web.id>
2015-11-16 13:43:43 +0100

    Add trait for Gentrap expression measures

---
+++ Gentrap/src/test/scala/nl/lumc/sasc/biopet/test/gentrap/ExpressionMeasures.scala
@@ -0,0 +1,62 @@
+package nl.lumc.sasc.biopet.test.gentrap
+
+import nl.lumc.sasc.biopet.test.SummaryPipeline.Executable
+import nl.lumc.sasc.biopet.test.{ Pipeline, SummaryPipeline }
+import nl.lumc.sasc.biopet.test.utils._
+
+/**
+  * Copyright (c) 2015 Leiden University Medical Center - Sequencing Analysis Support Core <sasc@lumc.nl>
+  *
+  * @author Wibowo Arindrarto <w.arindrarto@lumc.nl>
+  */
+trait ExpressionMeasures extends Pipeline {
+
+  def expressionMeasures: List[String] = Nil
+
+  val expressionMeasuresConfig =
+    if (expressionMeasures.nonEmpty)
+      Some(createTempConfig(Map("expression_measures" -> expressionMeasures)))
+    else None
+
+  override def configs = super.configs ++ expressionMeasuresConfig.toList
+}
+
+trait FragmentsPerGene extends ExpressionMeasures {
+  override def expressionMeasures = "fragments_per_gene" :: super.expressionMeasures
+  this match {
+    case s: SummaryPipeline => s.addExecutable(Executable("htseqcount", Some(""".+""".r)))
+    case _                  =>
+  }
+}
+
+trait BasesPerGene extends ExpressionMeasures {
+  override def expressionMeasures = "bases_per_gene" :: super.expressionMeasures
+}
+
+trait BasesPerExon extends ExpressionMeasures {
+  override def expressionMeasures = "bases_per_exon" :: super.expressionMeasures
+}
+
+trait CufflinksStrict extends ExpressionMeasures {
+  override def expressionMeasures = "cufflinks_strict" :: super.expressionMeasures
+  this match {
+    case s: SummaryPipeline => s.addExecutable(Executable("cufflinks", Some(""".+""".r)))
+    case _                  =>
+  }
+}
+
+trait CufflinksGuided extends ExpressionMeasures {
+  override def expressionMeasures = "cufflinks_guided" :: super.expressionMeasures
+  this match {
+    case s: SummaryPipeline => s.addExecutable(Executable("cufflinks", Some(""".+""".r)))
+    case _                  =>
+  }
+}
+
+trait CufflinksBlind extends ExpressionMeasures {
+  override def expressionMeasures = "cufflinks_blind" :: super.expressionMeasures
+  this match {
+    case s: SummaryPipeline => s.addExecutable(Executable("cufflinks", Some(""".+""".r)))
+    case _                  =>
+  }
+}
