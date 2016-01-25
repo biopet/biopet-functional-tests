4acb9d96065eec78b41732bb801218820096896f
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2016-01-25 20:25:06 +0100

    Added multiMappingSuccess to Gentrap

--- Gentrap/src/test/scala/nl/lumc/sasc/biopet/test/gentrap/GentrapSuccess.scala
+++ Gentrap/src/test/scala/nl/lumc/sasc/biopet/test/gentrap/GentrapSuccess.scala
@@ -1,10 +1,20 @@
 package nl.lumc.sasc.biopet.test.gentrap
 
-import nl.lumc.sasc.biopet.test.MultisampleSuccess
+import java.io.File
+
+import nl.lumc.sasc.biopet.test.MultisampleMappingSuccess
+import org.testng.annotations.Test
 
 /**
  * Created by pjvanthof on 05/11/15.
  */
-trait GentrapSuccess extends Gentrap with MultisampleSuccess {
-
+trait GentrapSuccess extends Gentrap with MultisampleMappingSuccess {
+  @Test
+  def testVariantcalling: Unit = {
+    val variantcallingDir = new File(outputDir, "variantcalling")
+    if (callVariants == Some(true)) {
+      assert(variantcallingDir.exists())
+      assert(variantcallingDir.isDirectory)
+    } else assert(!variantcallingDir.exists())
+  }
 }
