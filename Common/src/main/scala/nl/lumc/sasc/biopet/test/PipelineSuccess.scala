1f605392f221e062bb8ab07dd77be938ed180e5a
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2016-01-29 15:00:31 +0100

    Added DRMAA timeout test

--- Common/src/main/scala/nl/lumc/sasc/biopet/test/PipelineSuccess.scala
+++ Common/src/main/scala/nl/lumc/sasc/biopet/test/PipelineSuccess.scala
@@ -7,4 +7,5 @@ trait PipelineSuccess extends Pipeline {
   logMustNotHave("""Script failed with \d+ total jobs""".r)
   logMustHave("""Script completed successfully with \d+ total jobs""".r)
   logMustNotHave(""" - Input file does not exist:""".r)
+  logMustNotHave("""DrmaaJobRunner - Unable to determine status of job id""".r)
 }
