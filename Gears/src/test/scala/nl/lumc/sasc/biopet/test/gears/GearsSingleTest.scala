41f79eba393553e821275803fdeb7004a76cac45
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2016-02-05 11:32:47 +0100

    Added sample

--- Gears/src/test/scala/nl/lumc/sasc/biopet/test/gears/GearsSingleTest.scala
+++ Gears/src/test/scala/nl/lumc/sasc/biopet/test/gears/GearsSingleTest.scala
@@ -1,15 +1,20 @@
 package nl.lumc.sasc.biopet.test.gears
 
-import nl.lumc.sasc.biopet.test.samples.Mgm4457768_3_050Lib2
+import nl.lumc.sasc.biopet.test.samples.{ Mgm4459735_3_050Lib2, Mgm4457768_3_050Lib2 }
 
 /**
  * Created by pjvan_thof on 2/2/16.
  */
-class GearsSingleQiimeClosedTest extends GearsSuccess with Mgm4457768_3_050Lib2 {
+class GearsSingleQiimeClosedTest extends GearsSuccess with Mgm4457768_3_050Lib2 with Mgm4459735_3_050Lib2 {
   override def gearsUseKraken = Some(false)
   override def gearUseQiimeClosed = Some(true)
 }
 
-class GearsSingleKrakenTest extends GearsSuccess with Mgm4457768_3_050Lib2 {
+class GearsSingleKrakenTest extends GearsSuccess with Mgm4457768_3_050Lib2 with Mgm4459735_3_050Lib2 {
   override def gearsUseKraken = Some(true)
+}
+
+class GearsSingleKrakenQiimeTest extends GearsSuccess with Mgm4457768_3_050Lib2 with Mgm4459735_3_050Lib2 {
+  override def gearsUseKraken = Some(true)
+  override def gearUseQiimeClosed = Some(true)
 }
