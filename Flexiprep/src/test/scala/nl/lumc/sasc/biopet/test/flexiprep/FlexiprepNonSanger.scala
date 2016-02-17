da4b314c68721e552e0f53b1c084d6129604d7d5
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2016-02-17 11:33:13 +0100

    Updated md5sums

--- Flexiprep/src/test/scala/nl/lumc/sasc/biopet/test/flexiprep/FlexiprepNonSanger.scala
+++ Flexiprep/src/test/scala/nl/lumc/sasc/biopet/test/flexiprep/FlexiprepNonSanger.scala
@@ -14,7 +14,10 @@ class FlexiprepSingleNonSangerTest extends FlexiprepSingleDefaultTest with Flexi
   override def inputEncodingR1 = "solexa"
 
   /** MD5 checksum of the input file. */
-  override def md5SumInputR1 = "e85a903fad9267e4807a977292adc81f"
+  override def md5SumInputR1 = "332762ce0ea39a4bcb03de719d2229f2"
+
+  override def md5SumOutputR1 = Some("d082ebf2ded2fdae481b951f4f6e32ab")
+
 }
 
 class FlexiprepPairedNonSangerTest extends FlexiprepPairedDefaultTest with FlexiprepPairedClipTrimSummaryValues {
@@ -25,6 +28,9 @@ class FlexiprepPairedNonSangerTest extends FlexiprepPairedDefaultTest with Flexi
   override def inputEncodingR2 = "solexa"
 
   /** MD5 checksum of the input file. */
-  override def md5SumInputR1 = "e85a903fad9267e4807a977292adc81f"
-  override def md5SumInputR2 = Some("b5d471567542333e86533b1961d9501f")
+  override def md5SumInputR1 = "332762ce0ea39a4bcb03de719d2229f2"
+  override def md5SumInputR2 = Some("6519425e6191e136a4e29f4642ac0b84")
+
+  override def md5SumOutputR1 = Some("1d7f59553ea9fcd073acb5cbfc22156f")
+  override def md5SumOutputR2 = Some("8ea8ceef563c63b122be8ef9a8d10d2")
 }
