b0aef6233e2e98ce7b49b6b59e5df3cd169df006
bow <bow@bow.web.id>
2016-02-24 13:48:53 +0100

    Update map to foreach for summary schema tests

--- Common/src/main/scala/nl/lumc/sasc/biopet/test/SummaryPipeline.scala
+++ Common/src/main/scala/nl/lumc/sasc/biopet/test/SummaryPipeline.scala
@@ -49,7 +49,7 @@ trait SummaryPipeline extends PipelineSuccess with JValueMatchers {
   def testSummarySchema(): Unit =
     if (summarySchemaUrls.nonEmpty) {
       schemas should not be empty
-      schemas.map { s => s.validate(asJsonNode(summary)).iterator().asScala.toSeq shouldBe empty }
+      schemas.foreach { s => s.validate(asJsonNode(summary)).iterator().asScala.toSeq shouldBe empty }
     }
 
   @DataProvider(name = "summaryTests")
