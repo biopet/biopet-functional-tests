10031504d4fa9149296a89ef1739844fbab25764
Wai Yi Leung <w.y.leung@lumc.nl>
2016-05-19 08:45:01 +0200

    Fix value

--- Flexiprep/src/test/scala/nl/lumc/sasc/biopet/test/flexiprep/FlexiprepSingleClipTrimTest.scala
+++ Flexiprep/src/test/scala/nl/lumc/sasc/biopet/test/flexiprep/FlexiprepSingleClipTrimTest.scala
@@ -34,7 +34,7 @@ trait FlexiprepSingleClipTrimSummaryValues extends FlexiprepSingle {
       _ \ "1" \ "C" should inInterval(18.949536560247168, 0.01),
       _ \ "100" \ "A" should inInterval(25.630252100840334, 0.01),
       _ \ "100" \ "T" should inInterval(22.058823529411764, 0.01),
-      _ \ "100" \ "G" should inInterval(25.630252100840334, 0.01),
+      _ \ "100" \ "G" should inInterval(25.210084033613445, 0.01),
       _ \ "100" \ "C" should inInterval(27.100840336134453, 0.01)))
 
   addSummaryTest(statsFastqcR1QcPath :+ "adapters",
