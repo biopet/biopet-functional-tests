9866041ca782b36413f53cacd9f87522154f88c9
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2016-05-27 15:52:56 +0200

    Added basic Carp test

---
+++ Carp/src/test/scala/nl/lumc/sasc/biopet/test/carp/CarpSuccess.scala
@@ -0,0 +1,10 @@
+package nl.lumc.sasc.biopet.test.carp
+
+import nl.lumc.sasc.biopet.test.MultisampleMappingSuccess
+
+/**
+ * Created by pjvan_thof on 27-5-16.
+ */
+trait CarpSuccess extends Carp with MultisampleMappingSuccess {
+  //TODO: Add generic tests
+}
---
+++ Carp/src/test/scala/nl/lumc/sasc/biopet/test/carp/CarpTest.scala
@@ -0,0 +1,15 @@
+package nl.lumc.sasc.biopet.test.carp
+
+import nl.lumc.sasc.biopet.test.references.TestReference
+import nl.lumc.sasc.biopet.test.aligners.BwaMem
+import nl.lumc.sasc.biopet.test.samples.{ Chip1, Chip2 }
+
+/**
+ * Created by pjvan_thof on 27-5-16.
+ */
+class CarpTest extends CarpSuccess with BwaMem with TestReference with Chip1 with Chip2 {
+  def paired = true
+  def shouldHaveKmerContent = false
+
+  override def controls = super.controls ++ List("chip1" -> "chip2", "chip2" -> "chip1")
+}
