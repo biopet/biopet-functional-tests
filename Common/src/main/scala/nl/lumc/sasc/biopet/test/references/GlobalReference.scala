54effcb31ec56b9eb8bc14db1b8943d70d19693a
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2016-12-09 18:50:43 +0100

    Added new genomes

--- Common/src/main/scala/nl/lumc/sasc/biopet/test/references/GlobalReference.scala
+++ Common/src/main/scala/nl/lumc/sasc/biopet/test/references/GlobalReference.scala
@@ -34,5 +34,59 @@ trait GlobalReference extends Reference {
 
   /** This value should be in the global config */
   def hisat2Index: Option[String] = None
+}
 
+trait CelegansWBcel235 extends GlobalReference {
+  def referenceSpecies = Some("C.elegans")
+  def referenceName = Some("WBcel235")
+}
+
+trait ClfamiliarisCanFam3_1 extends GlobalReference {
+  def referenceSpecies = Some("C.l.familiaris")
+  def referenceName = Some("CanFam3.1")
+}
+
+trait HsapiensGRCh37 extends GlobalReference {
+  def referenceSpecies = Some("H.sapiens")
+  def referenceName = Some("GRCh37")
+}
+
+trait HsapiensGRCh37_p13_no_alt_analysis_set extends GlobalReference {
+  def referenceSpecies = Some("H.sapiens")
+  def referenceName = Some("GRCh37.p13_no_alt_analysis_set")
+}
+
+trait HsapiensGRCh38 extends GlobalReference {
+  def referenceSpecies = Some("H.sapiens")
+  def referenceName = Some("GRCh38")
+}
+
+trait HsapiensGRCh38_no_alt_analysis_set extends GlobalReference {
+  def referenceSpecies = Some("H.sapiens")
+  def referenceName = Some("GRCh38_no_alt_analysis_set")
+}
+
+trait HsapiensHg19 extends GlobalReference {
+  def referenceSpecies = Some("H.sapiens")
+  def referenceName = Some("hg19")
+}
+
+trait MmusculusGRCm38 extends GlobalReference {
+  def referenceSpecies = Some("M.musculus")
+  def referenceName = Some("GRCm38")
+}
+
+trait RnorvegicusRnor_6 extends GlobalReference {
+  def referenceSpecies = Some("R.norvegicus")
+  def referenceName = Some("Rnor_6.0")
+}
+
+trait DrerioGRCz10 extends GlobalReference {
+  def referenceSpecies = Some("D.rerio")
+  def referenceName = Some("GRCz10")
+}
+
+trait DmelanogasterBDGP6 extends GlobalReference {
+  def referenceSpecies = Some("D.melanogaster")
+  def referenceName = Some("BDGP6")
 }
--- Common/src/main/scala/nl/lumc/sasc/biopet/test/references/HsapiensGRCh37.scala
+++
@@ -1,9 +0,0 @@
-package nl.lumc.sasc.biopet.test.references
-
-/**
- * Created by pjvanthof on 14/11/15.
- */
-trait HsapiensGRCh37 extends GlobalReference {
-  def referenceSpecies = Some("H.sapiens")
-  def referenceName = Some("GRCh37")
-}
