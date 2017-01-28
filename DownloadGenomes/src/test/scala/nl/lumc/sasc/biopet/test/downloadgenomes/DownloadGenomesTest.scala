a68cdd7030810a6e3deb20f86a36ddfcae0dc06b
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-01-28 14:38:42 +0100

    Fixed a failing test

--- DownloadGenomes/src/test/scala/nl/lumc/sasc/biopet/test/downloadgenomes/DownloadGenomesTest.scala
+++ DownloadGenomes/src/test/scala/nl/lumc/sasc/biopet/test/downloadgenomes/DownloadGenomesTest.scala
@@ -1,12 +1,24 @@
 package nl.lumc.sasc.biopet.test.downloadgenomes
 
-import java.io.File
+import java.io.{ File, PrintWriter }
 
 /**
  * Created by pjvan_thof on 13-5-16.
  */
 class EcoliTest extends DownloadGenomes {
-  override def referenceConfigs = new File(resourcePath("/E.coli_K-12_MG1655.yml")) :: super.referenceConfigs
+  val assemblyReport = new File(resourcePath("/GCF_000005845.2.assembly.report.txt"))
+  val config = File.createTempFile("config.", ".yml")
+  config.deleteOnExit()
+  private val writer = new PrintWriter(config)
+  writer.println(
+    s"""
+      |E.coli:
+      |  K-12_MG1655:
+      |    ncbi_assembly_id:: ${assemblyReport.getAbsolutePath}
+    """.stripMargin)
+  writer.close()
+
+  override def referenceConfigs = config :: super.referenceConfigs
 }
 
 class CelegansTest extends DownloadGenomes {
