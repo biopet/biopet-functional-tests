2b89df17dc81b4600eb193ba83a5fe76f621a90b
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-03-09 13:06:05 +0100

    Fixing cutadapt test

--- Flexiprep/src/test/scala/nl/lumc/sasc/biopet/test/flexiprep/Flexiprep.scala
+++ Flexiprep/src/test/scala/nl/lumc/sasc/biopet/test/flexiprep/Flexiprep.scala
@@ -74,7 +74,7 @@ trait FlexiprepSuccessful extends FlexiprepRun with SummaryPipeline {
   if (r2.isDefined) addExecutable(Executable("fastqsync", Some(""".+""".r)))
   if (skipTrim != Some(true)) addExecutable(Executable("sickle", Some(""".+""".r)))
   else addNotHavingExecutable("sickle")
-  if (skipClip == Some(true)) addNotHavingExecutable("cutadapt")
+  if (skipClip == Some(true) || (!r1ContainAdapters && !r2ContainAdapters)) addNotHavingExecutable("cutadapt")
   else addExecutable(Executable("cutadapt", Some(""".+""".r)))
 
   override def summaryRoot = summaryLibrary(sampleId, libId)
