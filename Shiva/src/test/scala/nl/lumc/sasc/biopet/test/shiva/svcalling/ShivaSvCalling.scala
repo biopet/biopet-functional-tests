abfc79823789866e6afcd2fb8016894daaa4196f
akaljuvee <A.Kaljuvee@lumc.nl>
2017-03-13 10:15:06 +0100

    -

--- Shiva/src/test/scala/nl/lumc/sasc/biopet/test/shiva/svcalling/ShivaSvCalling.scala
+++ Shiva/src/test/scala/nl/lumc/sasc/biopet/test/shiva/svcalling/ShivaSvCalling.scala
@@ -14,11 +14,15 @@ trait ShivaSvCalling extends Pipeline {
 
   def bamFiles: List[File]
 
-  override def configs = super.configs.::(createTempConfig(Map("sv_callers" -> getSvCallersAsStrList)))
+  override def configs = super.configs.::(createTempConfig(Map("sv_callers" -> ShivaSvCalling.getSvCallersAsStrList(svCallers))))
 
   override def args = super.args ++ bamFiles.flatMap(x => Seq("-BAM", x.getAbsolutePath))
 
-  private def getSvCallersAsStrList(): List[String] = {
+}
+
+object ShivaSvCalling {
+
+  def getSvCallersAsStrList(svCallers: List[SvCaller]): List[String] = {
     var svCallersAsStr: List[String] = List()
     for (svCaller <- svCallers) svCallersAsStr = svCallersAsStr.::(svCaller.toString)
     svCallersAsStr
