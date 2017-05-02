6cc245a95399328bf979adff84b6295b752085ac
akaljuvee <A.Kaljuvee@lumc.nl>
2017-05-02 19:33:03 +0300

    a change related to parsing the text file specifying the locations of module schema files, the text file can now contain also prefixes of module names

--- Common/src/main/scala/nl/lumc/sasc/biopet/test/SummaryPipeline.scala
+++ Common/src/main/scala/nl/lumc/sasc/biopet/test/SummaryPipeline.scala
@@ -248,7 +248,7 @@ trait SummaryPipeline extends PipelineSuccess {
     val modulesUsed = Await.result(summaryDb.getModules(runId = Some(this.runId)), Duration.Inf)
     var moduleSchemas: Array[Array[Object]] = Array()
     for (module <- modulesUsed) {
-      SummaryPipeline.statisticsSchemas.find(schema => module.name.startsWith(schema._1)) match {
+      SummaryPipeline.moduleSchemas.find(schema => module.name.startsWith(schema._1)) match {
         case Some(schema) => moduleSchemas :+= Array(module.name, schema._2)
         case _            => println(s"No schema defined in conf for module ${module.name}") //logger.info(s"No schema defined in conf for module ${module.name}")
       }
