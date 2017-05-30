5a5b4f6641b2b919b91ffe8b417050994e1c7808
pjvan_thof <pjrvanthof@gmail.com>
2017-05-30 14:04:45 +0200

    Small improvements

--- Toucan/src/test/scala/nl/lumc/sasc/biopet/test/toucan/ToucanRuns.scala
+++ Toucan/src/test/scala/nl/lumc/sasc/biopet/test/toucan/ToucanRuns.scala
@@ -1,7 +1,5 @@
 package nl.lumc.sasc.biopet.test.toucan
 
-import java.io.File
-
 import nl.lumc.sasc.biopet.test.Biopet
 import nl.lumc.sasc.biopet.test.references._
 
@@ -11,21 +9,21 @@ import nl.lumc.sasc.biopet.test.references._
 class ToucanHsapiensHg19Test extends ToucanSuccess with HsapiensHg19 {
   override def vepVersion = Some("86")
   override def inputVcf =
-    Some(Biopet.fixtureFile("toucan" + File.separator + "two_vars_each_chrom_human.vcf.gz"))
+    Some(Biopet.fixtureFile("toucan", "two_vars_each_chrom_human.vcf.gz"))
   override def functionalTest = false
 }
 
 class ToucanHsapiensGRCh37Test extends ToucanSuccess with HsapiensGRCh37 {
   override def vepVersion = Some("86")
   override def inputVcf =
-    Some(Biopet.fixtureFile("toucan" + File.separator + "two_vars_each_chrom_human.vcf.gz"))
+    Some(Biopet.fixtureFile("toucan", "two_vars_each_chrom_human.vcf.gz"))
   override def functionalTest = false
 }
 
 class ToucanHsapiensGRCh37ScatterTest extends ToucanSuccess with HsapiensGRCh37 {
   override def vepVersion = Some("86")
   override def inputVcf =
-    Some(Biopet.fixtureFile("toucan" + File.separator + "two_vars_each_chrom_human.vcf.gz"))
+    Some(Biopet.fixtureFile("toucan", "two_vars_each_chrom_human.vcf.gz"))
   override def functionalTest = false
   override def enableScatter = true
 }
@@ -33,41 +31,41 @@ class ToucanHsapiensGRCh37ScatterTest extends ToucanSuccess with HsapiensGRCh37 
 class ToucanHsapiensGRCh38Test extends ToucanSuccess with HsapiensGRCh38 {
   override def vepVersion = Some("88")
   override def inputVcf =
-    Some(Biopet.fixtureFile("toucan" + File.separator + "two_vars_each_chrom_human.vcf.gz"))
+    Some(Biopet.fixtureFile("toucan", "two_vars_each_chrom_human.vcf.gz"))
   override def functionalTest = false
 }
 
 class ToucanMmusculusGRCm38Test extends ToucanSuccess with MmusculusGRCm38 {
   override def vepVersion = Some("86")
   override def inputVcf =
-    Some(Biopet.fixtureFile("toucan" + File.separator + "two_vars_each_chrom_human.vcf.gz"))
+    Some(Biopet.fixtureFile("toucan", "two_vars_each_chrom_human.vcf.gz"))
   override def functionalTest = false
 }
 
 class ToucanRnorvegicusRnor_6Test extends ToucanSuccess with RnorvegicusRnor_6 {
   override def vepVersion = Some("86")
   override def inputVcf =
-    Some(Biopet.fixtureFile("toucan" + File.separator + "two_vars_each_chrom_human.vcf.gz"))
+    Some(Biopet.fixtureFile("toucan", "two_vars_each_chrom_human.vcf.gz"))
   override def functionalTest = false
 }
 
 class ToucanCelegansWBcel235Test extends ToucanSuccess with CelegansWBcel235 {
   override def vepVersion = Some("86")
   override def inputVcf =
-    Some(Biopet.fixtureFile("toucan" + File.separator + "two_vars_each_chrom_human.vcf.gz"))
+    Some(Biopet.fixtureFile("toucan", "two_vars_each_chrom_human.vcf.gz"))
   override def functionalTest = false
 }
 
 class ToucanDrerioGRCz10Test extends ToucanSuccess with DrerioGRCz10 {
   override def vepVersion = Some("86")
   override def inputVcf =
-    Some(Biopet.fixtureFile("toucan" + File.separator + "two_vars_each_chrom_human.vcf.gz"))
+    Some(Biopet.fixtureFile("toucan", "two_vars_each_chrom_human.vcf.gz"))
   override def functionalTest = false
 }
 
 class ToucanDmelanogasterBDGP6Test extends ToucanSuccess with DmelanogasterBDGP6 {
   override def vepVersion = Some("86")
   override def inputVcf =
-    Some(Biopet.fixtureFile("toucan" + File.separator + "two_vars_each_chrom_human.vcf.gz"))
+    Some(Biopet.fixtureFile("toucan", "two_vars_each_chrom_human.vcf.gz"))
   override def functionalTest = false
 }
