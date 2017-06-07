83093525b8d6c5b4a755d8a812ca60b52885980c
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-06-07 09:24:47 +0200

    Fixing regexes

--- DownloadGenomes/src/test/scala/nl/lumc/sasc/biopet/test/downloadgenomes/ValidateAnnotationsTest.scala
+++ DownloadGenomes/src/test/scala/nl/lumc/sasc/biopet/test/downloadgenomes/ValidateAnnotationsTest.scala
@@ -11,18 +11,18 @@ class ValidateAnnotationsTest extends ValidateAnnotations with PipelineSuccess {
   @Test(dataProvider = "genomesProvider")
   def testAnnotation(species: String, genomeName: String): Unit =
     withClue(s"$species-$genomeName") {
-      testLogMustNotHave(s"Corrupt annotations files found for $species-$genomeName".r)
+      testLogMustNotHave(s"Corrupt annotations files found for $species-$genomeName${"$"}".r)
     }
 
   @Test(dataProvider = "genomesProvider")
   def testDbsnp(species: String, genomeName: String): Unit = withClue(s"$species-$genomeName") {
-    testLogMustNotHave(s"Corrupt vcf file found for $species-$genomeName".r)
+    testLogMustNotHave(s"Corrupt vcf file found for $species-$genomeName,".r)
   }
 
   @Test(dataProvider = "genomesProvider")
   def testMissingAnnotation(species: String, genomeName: String): Unit =
     withClue(s"$species-$genomeName") {
-      testLogMustNotHave(s"No features annotations found for $species-$genomeName".r)
+      testLogMustNotHave(s"No features annotations found for $species-$genomeName${"$"}".r)
     }
 
   @Test(dataProvider = "genomesProvider")
