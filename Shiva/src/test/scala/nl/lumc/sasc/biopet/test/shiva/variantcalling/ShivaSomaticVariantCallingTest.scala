b4331a73fcecf3697594b23f8e2f576ecdc8c6f7
akaljuvee <A.Kaljuvee@lumc.nl>
2017-06-21 21:05:45 +0200

    reformatting with scalafmt

--- Shiva/src/test/scala/nl/lumc/sasc/biopet/test/shiva/variantcalling/ShivaSomaticVariantCallingTest.scala
+++ Shiva/src/test/scala/nl/lumc/sasc/biopet/test/shiva/variantcalling/ShivaSomaticVariantCallingTest.scala
@@ -9,16 +9,26 @@ import nl.lumc.sasc.biopet.test.utils.createTempConfig
 
 class MuTect2Test extends ShivaVariantcallingWgs1 with MuTect2 {
 
-  override def bamFiles = super.bamFiles :+ Biopet.fixtureFile("samples", "wgs2", "wgs2.realign.bam")
+  override def bamFiles =
+    super.bamFiles :+ Biopet.fixtureFile("samples", "wgs2", "wgs2.realign.bam")
 
-  override def configs = super.configs :+ createTempConfig(Map("tumor_normal_pairs" -> List(Map("T" -> "wgs2", "N" -> "wgs1"))))
+  override def configs =
+    super.configs :+ createTempConfig(
+      Map("tumor_normal_pairs" -> List(Map("T" -> "wgs2", "N" -> "wgs1"))))
 
-  override def args = super.args ++
-    cmdConfig("run_contest", true) ++ cmdConfig("popfile", Biopet.fixtureFile("samples", "wgs2", "wgs2.vcf.gz"))
+  override def args =
+    super.args ++
+      cmdConfig("run_contest", true) ++ cmdConfig(
+      "popfile",
+      Biopet.fixtureFile("samples", "wgs2", "wgs2.vcf.gz"))
 
   override def testVariantcallerInfoTag(file: File): Unit = {}
 
-  addSettingsTest(SummaryGroup("shivavariantcalling"), List("somatic_variant_calling"), _ shouldBe true)
-  addSettingsTest(SummaryGroup("shivavariantcalling"), List("germline_variant_calling"), _ shouldBe false)
+  addSettingsTest(SummaryGroup("shivavariantcalling"),
+                  List("somatic_variant_calling"),
+                  _ shouldBe true)
+  addSettingsTest(SummaryGroup("shivavariantcalling"),
+                  List("germline_variant_calling"),
+                  _ shouldBe false)
 
 }
