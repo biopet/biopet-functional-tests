bf069171ab979c51ade4aadc958f7fbe152ac618
pjvan_thof <pjrvanthof@gmail.com>
2017-07-24 11:37:48 +0200

    Adding 2 duplicated samples

--- Basty/src/test/scala/nl/lumc/sasc/biopet/test/basty/BastySuccess.scala
+++ Basty/src/test/scala/nl/lumc/sasc/biopet/test/basty/BastySuccess.scala
@@ -1,11 +1,8 @@
 package nl.lumc.sasc.biopet.test.basty
 
-
 import nl.lumc.sasc.biopet.test.MultisampleMappingSuccess
 
 /**
   * Created by pjvan_thof on 27-5-16.
   */
-trait BastySuccess extends Basty with MultisampleMappingSuccess {
-
-}
+trait BastySuccess extends Basty with MultisampleMappingSuccess {}
--- Common/src/main/scala/nl/lumc/sasc/biopet/test/samples/Wgs2.scala
+++ Common/src/main/scala/nl/lumc/sasc/biopet/test/samples/Wgs2.scala
@@ -18,42 +18,60 @@ trait Wgs2Lib2 extends Samples {
   override def configs = super.configs :+ Wgs2.lib2ConfigFile
 }
 
-object Wgs2 {
-  val lib1ConfigMap = Map(
-    "samples" ->
-      Map(
-        "wgs2" ->
-          Map(
-            "libraries" ->
-              Map("lib1" ->
-                Map(
-                  "R1" -> Biopet
-                    .fixtureFile("samples", "wgs2", "wgs2-lib1_R1.fq.gz")
-                    .getAbsolutePath,
-                  "R1_md5" -> "6fb02af910026041f9ea76cd28968732",
-                  "R2" -> Biopet
-                    .fixtureFile("samples", "wgs2", "wgs2-lib1_R2.fq.gz")
-                    .getAbsolutePath,
-                  "R2_md5" -> "537ffc52342314d839e7fdd91bbdccd0"
-                )))))
-  val lib2ConfigMap = Map(
-    "samples" ->
-      Map(
-        "wgs2" ->
-          Map(
-            "libraries" ->
-              Map("lib2" ->
-                Map(
-                  "R1" -> Biopet
-                    .fixtureFile("samples", "wgs2", "wgs2-lib2_R1.fq.gz")
-                    .getAbsolutePath,
-                  "R1_md5" -> "df64e84fdc9a2d7a9301f2aac0071aee",
-                  "R2" -> Biopet
-                    .fixtureFile("samples", "wgs2", "wgs2-lib2_R2.fq.gz")
-                    .getAbsolutePath,
-                  "R2_md5" -> "47a65ad648ac08e802c07669629054ea"
-                )))))
+trait Wgs2Copy extends Samples with Wgs2CopyLib1 with Wgs2CopyLib2
 
-  val lib1ConfigFile = createTempConfig(lib1ConfigMap, "wgs2-lib1")
-  val lib2ConfigFile = createTempConfig(lib2ConfigMap, "wgs2-lib2")
+trait Wgs2CopyLib1 extends Samples {
+  override def samples = addSampleLibrary(super.samples, "wgs2_copy", "lib1")
+  override def configs = super.configs :+ Wgs2.lib1CopyConfigFile
+}
+
+trait Wgs2CopyLib2 extends Samples {
+  override def samples = addSampleLibrary(super.samples, "wgs2_copy", "lib2")
+  override def configs = super.configs :+ Wgs2.lib2CopyConfigFile
+}
+
+object Wgs2 {
+  def lib1ConfigMap(sampleName: String = "wgs2") =
+    Map(
+      "samples" ->
+        Map(
+          sampleName ->
+            Map(
+              "libraries" ->
+                Map("lib1" ->
+                  Map(
+                    "R1" -> Biopet
+                      .fixtureFile("samples", "wgs2", "wgs2-lib1_R1.fq.gz")
+                      .getAbsolutePath,
+                    "R1_md5" -> "6fb02af910026041f9ea76cd28968732",
+                    "R2" -> Biopet
+                      .fixtureFile("samples", "wgs2", "wgs2-lib1_R2.fq.gz")
+                      .getAbsolutePath,
+                    "R2_md5" -> "537ffc52342314d839e7fdd91bbdccd0"
+                  )))))
+  def lib2ConfigMap(sampleName: String = "wgs2") =
+    Map(
+      "samples" ->
+        Map(
+          sampleName ->
+            Map(
+              "libraries" ->
+                Map("lib2" ->
+                  Map(
+                    "R1" -> Biopet
+                      .fixtureFile("samples", "wgs2", "wgs2-lib2_R1.fq.gz")
+                      .getAbsolutePath,
+                    "R1_md5" -> "df64e84fdc9a2d7a9301f2aac0071aee",
+                    "R2" -> Biopet
+                      .fixtureFile("samples", "wgs2", "wgs2-lib2_R2.fq.gz")
+                      .getAbsolutePath,
+                    "R2_md5" -> "47a65ad648ac08e802c07669629054ea"
+                  )))))
+
+  lazy val lib1ConfigFile = createTempConfig(lib1ConfigMap(), "wgs2-lib1")
+  lazy val lib2ConfigFile = createTempConfig(lib2ConfigMap(), "wgs2-lib2")
+
+  lazy val lib1CopyConfigFile = createTempConfig(lib1ConfigMap("wgs2_copy"), "wgs2-lib1")
+  lazy val lib2CopyConfigFile = createTempConfig(lib2ConfigMap("wgs2_copy"), "wgs2-lib2")
+
 }
