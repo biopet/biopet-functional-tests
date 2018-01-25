0582ff197698dfb4e20523c6b2d3bd47fcc7cc2d
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2018-01-25 10:29:39 +0100

    Adding a single library sample
    (cherry picked from commit e2ed093)

--- Carp/src/test/scala/nl/lumc/sasc/biopet/test/carp/CarpTest.scala
+++ Carp/src/test/scala/nl/lumc/sasc/biopet/test/carp/CarpTest.scala
@@ -2,7 +2,7 @@ package nl.lumc.sasc.biopet.test.carp
 
 import nl.lumc.sasc.biopet.test.references.TestReference
 import nl.lumc.sasc.biopet.test.aligners.BwaMem
-import nl.lumc.sasc.biopet.test.samples.{Chip1, Chip2}
+import nl.lumc.sasc.biopet.test.samples.{Chip1, Chip2, Wgs1}
 
 /**
   * Created by pjvan_thof on 27-5-16.
@@ -12,4 +12,9 @@ class CarpTest extends CarpSuccess with BwaMem with TestReference with Chip1 wit
   def shouldHaveKmerContent = None
 
   override def controls = super.controls ++ List("chip1" -> "chip2", "chip2" -> "chip1")
+}
+
+class CarpRandomSampleTest extends CarpSuccess with BwaMem with TestReference with Wgs1 {
+  def paired = true
+  def shouldHaveKmerContent = None
 }
